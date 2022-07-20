d210c38812d5f0ee972ecb112dfe4065fbc44628
Mark Linn <marktlinn@gmail.com>
2022-07-20 17:22:59 +0200

    updated the server side code and added some basic HTML for test, now redirecting to Google Auth on the click of a button client-side

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
+const express = require("express")
+const app = express();
 require('dotenv').config();
+const PORT = 3000;
+
 const O_AUTH_ID = process.env.O_AUTH_ID;
 const O_AUTH_SECRET = process.env.O_AUTH_SECRET;
 const REDIRECT_URL = process.env.REDIRECT_URL;
 const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
 const O_Auth_2 = google.auth.OAuth2;
-console.log(O_AUTH_ID)
-const oAuth2Client = new O_Auth_2(O_AUTH_ID, O_AUTH_SECRET)
-oAuth2Client.setCredentials({refresh_token: REFRESH_TOKEN});
+// const oAuth2Client = new O_Auth_2(O_AUTH_ID, O_AUTH_SECRET, REDIRECT_URL)
+// oAuth2Client.setCredentials({refresh_token: REFRESH_TOKEN});
 
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 async function sendMail(name,receiver){
     try {
         const accessToken = await oAuth2Client.getAccessToken()
@@ -46,6 +53,36 @@ async function sendMail(name,receiver){
         return `Error with sendMail: ${error}`
     }
 }
+app.get("/", (req,res)=>{
+    res.sendFile(`${__dirname}/view/index.html`)
+})
+app.post("/login", (req,res)=>{
+    // res.send("<h1>hello<h1>")
+    // console.log(req)
+    res.sendFile(`${__dirname}/view/index.html`)
+    const oAuth2Client = new O_Auth_2(O_AUTH_ID, O_AUTH_SECRET, REDIRECT_URL)
+    const scopes = [
+        'https://www.googleapis.com/auth/drive.metadata.readonly'
+      ];
+    const authorizationUrl = oAuth2Client.generateAuthUrl({
+        // 'online' (default) or 'offline' (gets refresh_token)
+    access_type: 'online',
+        /** Pass in the scopes array defined above.
+          * Alternatively, if only one scope is needed, you can pass a scope URL as a string */
+    scope: scopes,
+        // Enable incremental authorization. Recommended as a best practice.
+    include_granted_scopes: true
+    });
+    console.log('In progress...')
+    res.redirect(authorizationUrl);
+})
 
-sendMail().then(result=> console.log(`Email sent: ${result}`))
-    .catch(error=> console.log(`Error sending: ${error}`))
+app.listen(PORT, ()=>{
+    console.log(`Server live on port: ${PORT}`)
+    }
+)
+
+// sendMail().then(result=> console.log(`Email sent: ${result}`))
+//     .catch(error=> console.log(`Error sending: ${error}`))
+// Send mail function working.
+
